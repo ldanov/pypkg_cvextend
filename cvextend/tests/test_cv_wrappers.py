4129d1ab00c642fb7984b2dbcda1f20e74a5ca3c
ldanov <ldanov@users.noreply.github.com>
2019-07-07 18:59:28 +0200

    reworked cv tools

--- cvextend/tests/test_cv_wrappers.py
+++ cvextend/tests/test_cv_wrappers.py
@@ -10,12 +10,15 @@ import pytest
 from imblearn.pipeline import Pipeline
 from sklearn.datasets import load_breast_cancer
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, f1_score, make_scorer
+from sklearn.model_selection import StratifiedKFold
 from sklearn.svm import SVC
 
 from hmeasure import h_score
-from sklearn.metrics import accuracy_score, f1_score, make_scorer
 
-from ..cv_utils import repeated_nested_cv
+from ..base import get_cv_grid
+from ..cv_wrappers import nested_cv
+from ..param_grid import generate_param_grid
 from ..score_grid import ScoreGrid
 
 # from imblearn.over_sampling import SMOTE
@@ -24,7 +27,7 @@ from ..score_grid import ScoreGrid
 def get_test1_settings():
     pipeline_steps = {'preprocessor': ['skip'],
                       'classifier': ['svm', 'rf']}
-    all_params_grid = {
+    params_dict = {
         'skip': {
             'pipe_step_instance': None
         },
@@ -49,41 +52,46 @@ def get_test1_settings():
          'score_criteria': 'min', 'scorer': make_scorer(f1_score)}
     ]
 
-    scorer_selection = ScoreGrid(scorer_selection_input)
-
     pipe = Pipeline([('preprocessor', None), ('classifier', None)])
+    param_grid, step_names = generate_param_grid(steps=pipeline_steps, 
+        param_dict=params_dict)
+    scorer_selection = ScoreGrid(scorer_selection_input)
+    cv_grid = get_cv_grid(estimator=pipe,
+                          param_grid=param_grid,
+                          scoring=scorer_selection.get_sklearn_dict(),
+                          cv=StratifiedKFold(shuffle=True, n_splits=5),
+                          verbose=1)
+    random_states = [0, 1]
+    outer_cv = StratifiedKFold(n_splits=2)
+
     X, y = load_breast_cancer(return_X_y=True)
 
     kwargs = {
+        'additional_info': {'data_name':"breast_cancer"},
+        'cv_grid': cv_grid,
         'X': X,
         'y': y,
-        'param_grid': all_params_grid,
-        'steps': pipeline_steps,
-        'pipe': pipe,
+        'step_names': step_names,
+        'random_states': random_states,
+        'outer_cv': outer_cv,
         'score_selection': scorer_selection
     }
     return kwargs
 
 
-def test_repeated_nested_cv():
-    expected_columns = ['data_name', 'estimator', 'n_repeat', 'outer_fold_n', 'params',
-                        'score_name', 'score_value', 'scorer', 'type_classifier',
-                        'type_preprocessor']
-    # two classifiers, two repeats, two inner folds and three scores
-    expected_min_rows = 2 * 2 * 2 * 3
+def test_nested_cv():
+    full_exp_cols = ['data_name', 'estimator', 'inner_cv_random_state', 'outer_fold_n',
+       'params', 'score_name', 'score_value', 'scorer', 'type_classifier',
+       'type_preprocessor']
+    # two classifiers, two inner folds and three scores
+    exp_min_rows_outer = 2 * 2 * 3
 
     input_settings = get_test1_settings()
 
-    result = repeated_nested_cv(
-        data_name="breast_cancer",
-        **input_settings,
-        n_repeats=2,
-        k_inner_folds=5,
-        k_outer_folds=2,
-        inner_cv_n_jobs=1,
-        verbose_in_cv=1,
-        verbose_out_cv=1
+    full_result, _ = nested_cv(
+       **input_settings
     )
-    df_out = pandas.DataFrame(result)
-    assert (df_out.columns == expected_columns).all()
-    assert df_out.shape[0] >= expected_min_rows
+    
+    df_full_result = pandas.DataFrame(full_result)
+    assert (df_full_result.columns == full_exp_cols).all()
+    assert df_full_result.shape[0] >= exp_min_rows_outer
