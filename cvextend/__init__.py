650aac23492229b5189a7d50f6a39e00d622da07
ldanov <18732847+ldanov@users.noreply.github.com>
2020-08-09 21:25:06 +0200

    moved version

--- cvextend/__init__.py
+++ cvextend/__init__.py
@@ -2,12 +2,11 @@
 cvextend extends sklearn's model_selection module.
 """
 
+from ._version import __version__
 from .cv_wrappers import basic_cv, nested_cv
 from .eval_grid import EvaluationGrid
 from .param_grid import generate_param_grid
 from .score_grid import ScoreGrid
-
-__version__ = 'v0.2.0'
 
 __all__ = [
     '__version__',
